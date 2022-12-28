dfae9da0dbf90c46dd3d02823c41a5cee8ed8882
luis arthur <luisarthurlards03@gmail.com>
2022-12-28 19:35:31 -0300

    gui para  a adição da senha

--- geradorDeChave/createKey.py
+++ geradorDeChave/createKey.py
@@ -2,9 +2,26 @@ from b64 import *
 from hashlib import md5
 import re
 import uuid
+import PySimpleGUI as sg
+
 
 codeMac = (':'.join(re.findall('..', '%012x' % uuid.getnode())))
-password = 'batata'
+layout = [
+        [
+            sg.Text('senha:'),
+            sg.InputText(background_color='#ffffff'),
+        ],
+        [sg.Button('verificar'), sg.Button('Cancelar')]]
+
+window = sg.Window('teste').Layout(layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancelar' or event == (
+    'verificar'):  # if user closes window or clicks cancel
+        password = input = values[0].replace('"', '')
+        break
+window.close()
 
 md5edKey = (f'{md5(password.encode()).hexdigest()}||{codeMac}')
 encodedKey = b64encode(md5edKey)
